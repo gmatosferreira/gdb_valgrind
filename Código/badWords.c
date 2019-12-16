6aa5b79ed7faa271c78ce089eb73ff3dd61f74f3
moraisandreua <48691769+moraisandreua@users.noreply.github.com>
2019-12-16 12:13:59 +0000

    Update badWords.c

--- Código/badWords.c
+++ Código/badWords.c
@@ -1,18 +1,44 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <ctype.h>
+#include <time.h>
 
 char * badWord(int, char*);
-
+char * badWord2(int, char*, int, int);
+char palavra[10000000];
 int main(int argc, char **argv){
     char s[100]="O joao` foi as compra's, comprou um umb'igo! falante por £233";
     int n = sizeof(s);
     
-    printf("%s\n", badWord(n, s));
+    //char* word = badWord(n, s);
+    //printf("%s\n", word);
+    
+    // badWord
+    clock_t t;
+    t = clock();
+    printf("Timer starts for badWord \n");
+    
+    badWord2(n, s, 0, 0);
+    printf("%s\n", palavra);
+    
+    t = clock() - t;
+    double time_taken = ((double)t)/CLOCKS_PER_SEC; // calculate the elapsed time
+    printf("The program took %f seconds to execute\n\n", time_taken);
+    
+    
+    // badWord2
+    t = clock();
+    printf("Timer starts for badWord2 \n");
+    
+    badWord2(n, s, 0, 0);
+    printf("%s\n", palavra);
+    
+    t = clock() - t;
+    time_taken = ((double)t)/CLOCKS_PER_SEC; // calculate the elapsed time
+    printf("The program took %f seconds to execute\n", time_taken);
 }
 
 char * badWord(int n, char s[n]){
-    char palavra[n];
     int index=0;
     for(int i=0; i<n; i++){
         char c = s[i];
@@ -24,4 +50,20 @@ char * badWord(int n, char s[n]){
         }
     }
     return palavra;
+}
+
+char * badWord2(int n, char s[n], int currentIndex, int currentCounter){
+    if(s[currentIndex]=='\0'){
+        return palavra;
+    }
+    char c = s[currentIndex];
+    char c2 = s[currentIndex+1];
+    if(isalnum(c) || (ispunct(c) && isalpha(c2))){
+        palavra[currentCounter]=c;
+        currentCounter++;
+    }
+    
+    currentIndex++;
+    badWord2(n, s, currentIndex, currentCounter);
+    
 }
